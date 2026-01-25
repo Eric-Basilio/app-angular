f8ea1285252b2269897bf5adb4e7b1a8ef977e25
Eric Basilio <eric__basilio@outlook.com>
2026-01-24 22:49:07 -0300

    binding de estilos css

--- src/app/pages/binding.page.component.ts
+++ src/app/pages/binding.page.component.ts
@@ -1,15 +1,38 @@
 import { Component } from "@angular/core";
 
-@Component({
-    selector: "app-binding-page",
-    template: `
-    <h1> Imagem do Angular </h1>
-    <img [src]= "imagem" [style]="{width: width}"/>`
-})
+@Component(
+    {
+        selector: "app-binding-page",
+        template: `
+        <h1> Imagem do Angular </h1>
+        <img [src]= "imagem" [style]="{width: width}"/>
+        <div class="alert" [class]="{sucess: sucesso}">
+            ALERTA
+        </div>
+        <button (click)="enviarDados()">Enviar Dados</button>
+        `,
+        styles: [
+            `.alert{
+                width: 200px;
+                height: 100px;
+                border: 1px solid blue;
+            }
+            .sucess {
+                background-color: lightgreen;
+
+            }
+            `
+        ]
+    }
+)
 
 export class BindingPageComponent {
 
 imagem = "https://angular.io/assets/images/logos/angular/angular.svg";
 width = "300px";
+sucesso = true;
 
+enviarDados() {
+    this.sucesso = !this.sucesso;
+};
 }

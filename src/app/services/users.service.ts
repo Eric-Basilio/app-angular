34228fc4e219533a183e2dd2f05118fb0d52d940
Eric Basilio <eric__basilio@outlook.com>
2026-01-29 19:10:33 -0300

    criação da api-rest

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import {User} from "../model/user";
+const URL = "https://gorest.co.in/public/v2/users"
+
+@Injectable({
+    providedIn: 'root'
+})
+
+
+
+export class UserService {
+
+constructor(private http: HttpClient){}
+
+getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(URL)
+}
+
+postUser(user: User){
+    this.http.post(URL, user)
+    .subscribe(response => {
+        console.log('Salvo')
+    }, (error) => {
+        console.error(error)
+    })
+}
+
+deleteUser(id:number){
+    this.http.delete(`${URL}/${id}`)
+    .subscribe(response =>{
+        console.log('Deletado')
+    }, (error) => {
+        console.error(error)
+    })
+}
+
+updateUser(id:number, user: User){
+    this.http.put(`${URL}/${id}`, user)
+    .subscribe(response =>{
+        console.log('Nome atualizado')
+    }, (error) => {
+        console.error(error)
+    })
+}
+
+putUser(id: number, user: User){
+    this.http.put(`${URL}/${id}`, user)
+    .subscribe(_ => {
+        console.log('usuario salvo')
+    }, (error) => {
+        console.error(error)
+    })
+}
+
+}
